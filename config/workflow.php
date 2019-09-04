b3f86fa461bed9aa6f6f8780ca08d2cf27ff4e31
DmVsk <dm.vsk@yandex.ru>
2019-09-04 16:06:25 +0300

    Migration

---
+++ config/workflow.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by black40x@yandex.ru
+ * Date: 04/09/2019
+ */
+
+
+return [
+
+    'pin' => [
+        'life_time' => env('APP_PINCODE_LIFETIME', 5) * 60
+    ],
+
+];
--- database/migrations/2019_09_03_124138_create_user_jwt_tokens_table.php
+++ database/migrations/2019_09_03_124138_create_user_jwt_tokens_table.php
@@ -18,10 +18,11 @@ class CreateUserJwtTokensTable extends Migration
     {
         Schema::create('user_jwt_tokens', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->passthru('bigint', 'user_id')->unsigned();
             $table->passthru('varchar', 'access_token', 'varchar(255)');
             $table->passthru('varchar', 'refresh_token', 'varchar(255)');
-            $table->passthru('bigint', 'user_id')->unsigned();
-            $table->passthru('bigint', 'expires_in')->unsigned();
+            $table->passthru('bigint', 'access_expire_at')->unsigned();
+            $table->passthru('bigint', 'refresh_expire_at')->unsigned();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
