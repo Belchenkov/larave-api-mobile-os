513d6c0a86f8d683a6204eeeef118cd055ed15b0
DmVsk <dm.vsk@yandex.ru>
2019-09-26 16:10:03 +0300

    User catalog

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,25 @@
 |
 */
 
+use App\Http\Resources\Api\v1\User\UserCatalog;
 use App\Models\Transit\_1C\Transit1cEmployee;
+use App\Repositories\User\UserRepository;
 
 Route::get('/', function () {
     return response()->json([
         'result' => true,
         'data' => 'Welcome to GKOsnova Mobile Server'
     ]);
+});
+
+
+Route::get('/test', function () {
+    $userRepository = new UserRepository();
+    //dd($userRepository->getUserCatalog()->limit(20)->get());
+    return UserCatalog::collection(
+        $userRepository->getUserCatalog(request()->get('search'))
+            ->simplePaginate(50)->appends([
+                'search' => request()->get('search')
+            ])
+    );
 });
