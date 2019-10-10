1e3b2629626e2b9c884c6c55f16dffadf3af10d2
DmVsk <dm.vsk@yandex.ru>
2019-10-10 14:10:05 +0300

    Unit testing

--- app/Services/Auth/JwtGuard.php
+++ app/Services/Auth/JwtGuard.php
@@ -34,10 +34,12 @@ class JwtGuard implements Guard
 
         $user = null;
 
-        if ($token = UserJwtToken::where('access_token', request()->bearerToken())
-            ->where('access_expire_at', '>', Carbon::now())->first()) {
+        if (request()->bearerToken()) {
+            if ($token = UserJwtToken::where('access_token', request()->bearerToken())
+                ->where('access_expire_at', '>', Carbon::now())->first()) {
                 $user = $token->user;
             }
+        }
 
         return $this->user = $user;
     }
