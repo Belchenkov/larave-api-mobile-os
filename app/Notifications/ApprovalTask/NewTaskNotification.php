2af3e08a4f9272fba021f18b53326dde902e843b
Алексей Бельченков <u608110@gmail.com>
2019-10-25 18:46:26 +0400

    Fix bags for NewTaskNotification

--- app/Notifications/ApprovalTask/NewTaskNotification.php
+++ app/Notifications/ApprovalTask/NewTaskNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class NewTaskNotification extends Notification
+class NewTaskNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
